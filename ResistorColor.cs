82bcc4a6c71db5b4452a577834734797272bb192
Audrius Budrys <budrys@email.com>
2021-02-06 07:29:16 +0200

    Add project files.

---
+++ ResistorColor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public static class ResistorColor
+{
+    static string[] colorCodes = new string[] { "black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white" };
+    public static int ColorCode(string color)
+    {
+        int i = 0;
+        foreach (string item in colorCodes)
+        {
+            if (color == item)
+            {
+                break;
+            }
+
+            i++;
+        }
+
+        return i;
+    }
+
+    public static string[] Colors()
+    {
+        return colorCodes;
+    }
+}
